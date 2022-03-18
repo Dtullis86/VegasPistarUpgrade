ca90cab5279f86ebe63358611cf9380d1a577b17
Dtullis86 <Dtullis86@gmail.com>
2022-03-18 13:43:02 -0700

    Update w0chpappinstall.sh

--- w0chpdashboard/w0chpappinstall.sh
+++ w0chpdashboard/w0chpappinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #add files specifically for w0chp dashboard
-sudo cp /home/pi-star/VegasPistarUpgrade/w0chpdashboard/hslivecall.desktop /usr/share/applications/hslivecall.desktop
+sudo cp /home/pi-star/VegasPistarUpgrade/desktopfiles/hslivecall.desktop /usr/share/applications/hslivecall.desktop
 sudo cp /home/pi-star/VegasPistarUpgrade/w0chpdashboard/bmapi.key /etc/bmapi.key 
