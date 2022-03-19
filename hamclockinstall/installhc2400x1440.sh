36b9c7e244116cafa74d4b3780856225b6f2e8f0
Dtullis86 <Dtullis86@gmail.com>
2022-03-19 14:57:49 -0700

    Update installhc2400x1440.sh

--- hamclockinstall/installhc2400x1440.sh
+++ hamclockinstall/installhc2400x1440.sh
@@ -1,8 +1,6 @@
 
 #!/bin/bash
 
-sudo apt-get update && sudo apt install -y g++ libx11-dev xserver-xorg raspberrypi-ui-mods lightdm lxsession openssl
-
 rm -fr ESPHamClock
 curl -O https://www.clearskyinstitute.com/ham/HamClock/ESPHamClock.zip
 unzip ESPHamClock.zip
